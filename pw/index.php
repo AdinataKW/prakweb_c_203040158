b65e3d1f5058a67e0bc3257af5a21f2e5ce3f725
acilacil <adinata2682@gmail.com>
2022-10-03 21:39:07 +0700

    DONE

--- pw/index.php
+++ pw/index.php
@@ -10,7 +10,7 @@ $buku = query("SELECT * FROM buku");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DAFTAR BUKU</title>
+    <title>ACIL</title>
     <style>
         h3{
             margin: 0 auto;
